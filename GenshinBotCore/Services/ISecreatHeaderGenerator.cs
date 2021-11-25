e9df22936fc705d68efa8723ee2388d6714db676
神里绫华 <1137361788@qq.com>
2021-11-25 09:45:35 +0800

    [feat] abstract takumi api

---
+++ GenshinBotCore/Services/ISecreatHeaderGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenshinBotCore.Services
+{
+    public interface ISecreatHeaderGenerator
+    {
+        IDictionary<string, string> GenerateSecretHeader();
+    }
+}
---
+++ GenshinBotCore/Services/ITakumiApi.cs
@@ -0,0 +1,18 @@
+﻿using GenshinBotCore.Models.TakumiApi;
+using Index = GenshinBotCore.Models.TakumiApi.Index;
+
+namespace GenshinBotCore.Services
+{
+    internal interface ITakumiApi
+    {
+        public IApiResponse<UserToken> GetMultiTokenByLoginTicket(string loginTicket, string uid, int tokenType);
+
+        public IApiResponse<GameRoles> GetGameRolesBySToken(string stoken, string uid);
+
+        public IApiResponse<SpiralAbyss> GetSpiralAbyss(string roleId, string serverId, int scheduleType);
+
+        public IApiResponse<DailyNote> GetDailyNote(string roleId, string serverId);
+
+        public IApiResponse<Index> GetIndex(string roleId, string serverId);
+    }
+}
