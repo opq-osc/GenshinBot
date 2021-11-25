db678eabb55754fa9eed298b7b70096bf0676b5c
神里绫华 <1137361788@qq.com>
2021-11-25 11:16:03 +0800

    [feat] add testing project

---
+++ GenshinBotCoreTests/Services/TakumiSecretHeaderGeneratorTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GenshinBotCore.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenshinBotCore.Services.Tests
+{
+    [TestClass()]
+    public class TakumiSecretHeaderGeneratorTests
+    {
+        [TestMethod()]
+        public void GenerateSecretHeaderTest()
+        {
+            TakumiSecretHeaderGenerator takumiSecretHeaderGenerator = new TakumiSecretHeaderGenerator((config) =>
+            {
+                config.Salt = "xV8v4Qu54lUKrEYFZkJhB8cuOh9Asafs";
+            });
+            var header = takumiSecretHeaderGenerator.GenerateSecretHeader();
+            Assert.IsTrue(header.Any());
+        }
+    }
+}
