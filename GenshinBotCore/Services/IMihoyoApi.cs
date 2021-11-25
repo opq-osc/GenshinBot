f2dd36ce62409c61152996132568aa90dfbee6d5
神里绫华 <1137361788@qq.com>
2021-11-25 12:32:54 +0800

    [feat] add abstract login api

---
+++ GenshinBotCore/Services/IMihoyoApi.cs
@@ -0,0 +1,10 @@
+﻿using GenshinBotCore.Models;
+using GenshinBotCore.Models.MihoyoAccount;
+
+namespace GenshinBotCore.Services
+{
+    public interface IMihoyoApi
+    {
+        public Task<IApiResponse<Account>> Login(string phone, string captcha);
+    }
+}
