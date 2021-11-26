8b73271de3d8604cbf099c2f5af198475e0fdf57
神里绫华 <1137361788@qq.com>
2021-11-26 10:51:47 +0800

    [fix] add qq to user entity

--- GenshinBotCore/Entities/User.cs
+++ GenshinBotCore/Entities/User.cs
@@ -9,6 +9,9 @@ namespace GenshinBotCore.Entities
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
 
+        [Required]
+        public long QQ { get; set; }
+
         public string MihoyoId { get; set; } = string.Empty;
 
         public string GenshinUid { get; set; } = string.Empty;
