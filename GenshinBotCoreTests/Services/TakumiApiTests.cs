06f8e8ee3107acb1ae7685da94cac8402f79e0fa
刻晴 <1137361788@qq.com>
2021-11-28 13:02:44 +0800

    [test] add unit test for index api

--- GenshinBotCoreTests/Services/TakumiApiTests.cs
+++ GenshinBotCoreTests/Services/TakumiApiTests.cs
@@ -22,13 +22,40 @@ namespace GenshinBotCore.Services.Tests
                         config.AppVersion = "2.16.1";
                         config.ClientType = 5;
                         config.Salt = "xV8v4Qu54lUKrEYFZkJhB8cuOh9Asafs";
-                    }), 
+                    }),
                 config =>
                 {
                     config.BaseUrl = "https://api-takumi.mihoyo.com";
                     config.GameAccountsUrl = "/game_record/app/card/wapi/getGameRecordCard";
                 });
-            var result = takumiApi.GetGameAccounts("296714852").Result;
+            var userManager = new TestUserManager();
+            var user = userManager.GetUserByQQ(1137361788);
+            Assert.IsNotNull(user);
+            var result = takumiApi.GetGameAccounts(user.MihoyoId).Result;
+
+            Assert.IsTrue(result.IsSuccess);
+        }
+
+        [TestMethod()]
+        public void GetIndexAsyncTest()
+        {
+            var takumiApi = new TakumiApi(new TestUserManager(), new TakumiSecretHeaderGenerator
+                (
+                    new TestUserManager(), new TestSecretManager(), config =>
+                    {
+                        config.AppVersion = "2.16.1";
+                        config.ClientType = 5;
+                        config.Salt = "xV8v4Qu54lUKrEYFZkJhB8cuOh9Asafs";
+                    }),
+                config =>
+                {
+                    config.BaseUrl = "https://api-takumi.mihoyo.com";
+                    config.IndexUrl = "/game_record/app/genshin/api/index";
+                });
+            var userManager = new TestUserManager();
+            var user = userManager.GetUserByQQ(1137361788);
+            Assert.IsNotNull(user);
+            var result = takumiApi.GetIndexAsync(user.GenshinUid, "cn_gf01").Result;
 
             Assert.IsTrue(result.IsSuccess);
         }
