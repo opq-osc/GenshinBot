e900f4646e8f0eec3e1ead153ff008cd3421414f
神里绫华 <1137361788@qq.com>
2021-11-30 09:15:08 +0800

    [feat] add wpf project to design views

---
+++ GenshinBot.Design/Pages/OverView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GenshinBot.Design.Pages
+{
+    /// <summary>
+    /// OverView.xaml 的交互逻辑
+    /// </summary>
+    public partial class OverView : Page
+    {
+        public OverView()
+        {
+            InitializeComponent();
+        }
+    }
+}
