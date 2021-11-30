86940d436f5545402c2572bc5d162423075fe90c
神里绫华 <1137361788@qq.com>
2021-11-30 16:22:32 +0800

    [test] add unit test for renderer

---
+++ GenshinBotCoreTests/Mocks/TestRender3.cs
@@ -0,0 +1,19 @@
+﻿using GenshinBotCore.View;
+using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenshinBotCoreTests.Mocks
+{
+    public class TestRender3 : ViewRenderer
+    {
+        public TestRender3(SKCanvas canvas) : base(canvas)
+        {
+            this.AddSubView<TestRender1>(SKRect.Create(0, 0, 200, 200));
+            this.AddSubView<TestRender2>(SKRect.Create(0, 0, 200, 200));
+        }
+    }
+}
---
+++ GenshinBotCoreTests/View/ViewRendererTests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GenshinBotCore.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SkiaSharp;
+using GenshinBotCoreTests.Mocks;
+using System.IO;
+
+namespace GenshinBotCore.View.Tests
+{
+    [TestClass()]
+    public class ViewRendererTests
+    {
+        [TestMethod()]
+        public void SingleRendererTest1()
+        {
+            var bitmap = new SKBitmap(200, 200);
+            var canvas = new SKCanvas(bitmap);
+            var renderer = new TestRender1(canvas);
+
+            renderer.Render();
+
+           
+            var image = SKImage.FromBitmap(bitmap);
+            var file = File.Open("SingleRendererTest1.png", FileMode.Create);
+            image.Encode(SKEncodedImageFormat.Png, 90).SaveTo(file);
+            file.Close();
+
+            Assert.IsTrue(File.Exists("SingleRendererTest1.png"));
+        }
+
+        [TestMethod()]
+        public void SingleRendererTest2()
+        {
+            var bitmap = new SKBitmap(200, 200);
+            var canvas = new SKCanvas(bitmap);
+            var renderer = new TestRender2(canvas);
+
+            renderer.Render();
+
+
+            var image = SKImage.FromBitmap(bitmap);
+            var file = File.Open("SingleRendererTest2.png", FileMode.Create);
+            image.Encode(SKEncodedImageFormat.Png, 90).SaveTo(file);
+            file.Close();
+
+            Assert.IsTrue(File.Exists("SingleRendererTest2.png"));
+        }
+
+        [TestMethod()]
+        public void CombinedRendererTest()
+        {
+            var bitmap = new SKBitmap(200, 200);
+            var canvas = new SKCanvas(bitmap);
+            var renderer = new TestRender3(canvas);
+
+            renderer.Render();
+
+            var image = SKImage.FromBitmap(bitmap);
+            var file = File.Open("SingleRendererTest3.png", FileMode.Create);
+            image.Encode(SKEncodedImageFormat.Png, 90).SaveTo(file);
+            file.Close();
+
+            Assert.IsTrue(File.Exists("SingleRendererTest3.png"));
+        }
+    }
+}
