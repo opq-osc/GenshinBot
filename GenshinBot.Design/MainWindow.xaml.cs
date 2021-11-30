ff2fc27914bedec49cdd35d5fe792b7cf2583a91
神里绫华 <1137361788@qq.com>
2021-11-30 13:54:57 +0800

    [feat] add test renderer

--- GenshinBot.Design/MainWindow.xaml.cs
+++ GenshinBot.Design/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using GenshinBot.Design.Pages;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,20 @@ namespace GenshinBot.Design
         public MainWindow()
         {
             InitializeComponent();
+            var page = new OverView();
+            frame.Content = page;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            RenderTargetBitmap renderTargetBitmap = new RenderTargetBitmap(1080, 2340, 192, 192, PixelFormats.Pbgra32);
+            renderTargetBitmap.Render(frame.Content as Visual);
+            PngBitmapEncoder encoder = new PngBitmapEncoder();
+            encoder.Frames.Clear();
+            encoder.Frames.Add(BitmapFrame.Create(renderTargetBitmap));
+            var fs = File.Open("overview.png", FileMode.Create);
+            encoder.Save(fs);
+            fs.Close();
         }
     }
 }
