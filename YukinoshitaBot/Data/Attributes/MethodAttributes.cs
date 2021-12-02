a5b1354794475cda4bbda24eaf3c12d1e21a38bb
云堇 <saicem@foxmail.com>
2021-12-02 11:18:07 +0800

    [style] put all method attributes in one file

---
+++ YukinoshitaBot/Data/Attributes/MethodAttributes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YukinoshitaBot.Data.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class FriendTextAttribute : Attribute { }
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public class GroupTextAttribute : Attribute { }
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public class TempTextAttribute : Attribute { }
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public class FriendImageAttribute : Attribute { }
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public class GroupImageAttribute : Attribute { }
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public class TempImageAttribute : Attribute { }
+}
